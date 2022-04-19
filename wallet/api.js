87c5b4d8a224ff08e8ac58e89f97f467b66bb008
Daniel Olano <me@olanod.com>
2022-04-19 16:01:23 +0200

    Send responses to parent window

--- wallet/api.js
+++ wallet/api.js
@@ -1,37 +1,97 @@
-const reqChan = new BroadcastChannel("req_channel");
-const resChan = new BroadcastChannel("res_channel");
+self.addEventListener("message", onMessage);
 
-addEventListener("message", onMessage);
-reqChan.addEventListener("message", onMessage);
-
+const CACHE = "x";
 const registry = new Map();
+const txt = new TextEncoder();
+let chan;
 
-async function onMessage({ data: { meta, data } }) {
-  switch (meta.plugin) {
+const onRequest = async ({ data: req }) => {
+  let output = await onMessage({ data: reqToMsg(req) });
+  let headers = new Headers(req.headers);
+  let cache = await caches.open(CACHE);
+
+  let id = headers.get("x-request-id");
+  let res = dataToResponse(output);
+  cache.put(id, res);
+  chan.postMessage(id);
+};
+
+const reqId = async (method, url) => {
+  let h = await crypto.subtle.digest("SHA-1", txt.encode(method + url));
+  return btoa(String.fromCharCode(...new Uint8Array(h)));
+};
+
+const dataToResponse = (data) => {
+  if (!data) return Response.redirect(`/`, 303);
+  if (typeof data == "string") return new Response(data);
+  if (data instanceof ArrayBuffer) {
+    return new Response(data, {
+      headers: { "Content-Type": "application/octet-stream" },
+    });
+  }
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+async function onMessage({ data: { plugin, action, meta, data } }) {
+  switch (plugin) {
     case undefined:
     case "_":
-      switch (meta.action) {
+      switch (action) {
+        case "connect":
+          chan = data;
+          chan.onmessage = onRequest;
+          break;
         case "register":
           for (let { name, url, config } of data) {
             let mod = await import(url);
             if (mod._create) await mod._create(config);
             registry.set(name, mod);
           }
-          console.info(
-            `loaded ${registry.size} plugin${registry.size > 1 ? "s" : ""}`,
-          );
+          let s = registry.size;
+          console.debug(`loaded ${s} plugin${s > 1 ? "s" : ""}`);
       }
       break;
     default:
-      let p = registry.get(meta.plugin);
-      if (p && p[meta.action]) {
-        let data = p[meta.action]();
-        resChan.postMessage({ meta: { id: meta.id }, data });
-      } else {
-        resChan.postMessage({
-          meta: { status: 404, id: meta.id },
-          data: `${meta.plugin}->${meta.action} not found`,
-        });
-      }
+      let p = registry.get(plugin);
+      return p ? p[action]?.(data) : null;
   }
+}
+
+const reqToMsg = ({ method, url, headers, body }) => {
+  let [plugin, action, params] = pluginAction(method, url);
+  let meta = toObjLiteral(headers);
+  meta.url = url;
+  return {
+    plugin,
+    action,
+    meta,
+    data: method == "GET" || method == "HEAD" ? params : body,
+  };
+};
+
+const pluginAction = (m, u) => {
+  m = m == "POST" ? "create" : m.toLowerCase();
+  let url = new URL(u);
+  let path = url.pathname.replace("/", "").split("/");
+  return [
+    path.shift(),
+    camelize([m, ...path].join(" ")),
+    toObjLiteral(url.searchParams),
+  ];
+};
+
+function camelize(str) {
+  return str.replace(
+    /(?:^\w|[A-Z]|\b\w)/g,
+    (word, index) => index === 0 ? word.toLowerCase() : word.toUpperCase(),
+  ).replace(/\s+/g, "");
+}
+
+function toObjLiteral(iter) {
+  return [...iter].reduce((o, [k, v]) => {
+    o[k] = v;
+    return o;
+  }, {});
 }
--- wallet/sw.js
+++ wallet/sw.js
@@ -1,101 +1,61 @@
 self.addEventListener("install", (e) => e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", (e) => e.respondWith(handleRequest(e.request)));
+self.addEventListener("message", ({ data: { port } }) => {
+  port.onmessage = onResponse;
+  workers.unshift(port);
+});
+
+const workers = [];
+const pendingRequests = new Map();
+const TIMEOUT = 5 * 1_000;
+const txt = new TextEncoder();
+
+const handleRequest = async (req) =>
+  hasWhitelistedExt(new URL(req.url).pathname) ? fetch(req) : processInWorker(req);
 
 const hasWhitelistedExt = (url) =>
   ["/", ".html", ".css", ".ico", ".js", ".wasm", ".json"].some((ext) =>
     url.endsWith(ext)
   );
 
-const toObjLiteral = (iter) =>
-  [...iter].reduce((o, [k, v]) => {
-    o[k] = v;
-    return o;
-  }, {});
-
-const pluginAction = (m, u) => {
-  m = m == "POST" ? "create" : m;
-  let url = new URL(u);
-  let path = url.pathname.replace("/", "").split("/");
-  return [
-    path.shift(),
-    camelize([m.toLowerCase(), ...path].join(" ")),
-    toObjLiteral(url.searchParams),
-  ];
+const reqId = (method, url) => {
+  let h = crypto.subtle.digest("SHA-1", txt.encode(method + url));
+  return btoa(String.fromCharCode(...new Uint8Array(h)));
 };
 
-const reqToTransferable = async (req) => {
-  let [plugin, action, params] = pluginAction(req.method, req.url);
-  let msg = {
-    meta: { plugin, action, ...toObjLiteral(req.headers) },
-  };
-  msg.data = req.method == "GET" || req.method == "HEAD"
-    ? params
-    : await req.arrayBuffer();
-  return msg;
-};
-
-const handleRequest = (req) =>
-  hasWhitelistedExt(req.url) ? fetch(req) : broadcastAndWaitResponse(req);
-
-const TIMEOUT = 5000;
 const timeout = (ms) => new Promise((res) => setTimeout(res, ms));
-const timeoutResponse = (id) =>
-  timeout(TIMEOUT).then(() => {
-    pendingRequests.delete(id);
-    return new Response("Timeout!", { status: 504 });
-  });
+const timeoutResponse = async (id) => {
+  await timeout(TIMEOUT);
+  pendingRequests.delete(id);
+  return new Response("Request timed out", { status: 504 });
+};
 
-async function broadcastAndWaitResponse(req) {
-  let msg = await reqToTransferable(req);
-  const id = uuidv4();
-  msg.meta.id = id;
+// transfer request to be processed by plugin worker
+const processInWorker = async (req) => {
+  let method = req.method,
+    url = req.url,
+    headers = [...req.headers],
+    body = await req.arrayBuffer();
+  let id = crypto.randomUUID();
+  headers.push(["x-request-id", id]);
+  workers[0]?.postMessage({ method, url, headers, body }, [body]);
 
-  reqChan.postMessage(msg);
-
-  let resolve;
-  const response = new Promise((r) => {
-    resolve = r;
-  });
+  let resolve,
+    response = new Promise((r) => {
+      resolve = r;
+    });
   pendingRequests.set(id, resolve);
 
   return Promise.race([timeoutResponse(id), response]);
-}
+};
 
-const reqChan = new BroadcastChannel("req_channel");
-const resChan = new BroadcastChannel("res_channel");
-const pendingRequests = new Map();
-
-resChan.onmessage = ({ data }) => {
-  const id = data?.meta?.id;
-  if (!id) {
-    console.debug("request without id");
-    return;
-  }
-  delete data.meta.id;
-
+// handle answers coming from plugin worker
+const onResponse = async ({ data: id }) => {
   const resolve = pendingRequests.get(id);
   if (!resolve) {
     console.debug(`no matching request for ${id}`);
     return;
   }
-
-  resolve(new Response(data.data, { ...data.meta }));
+  resolve(Response.redirect(`/?rid=${id}`, 303));
 };
-
-function uuidv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(
-    /[018]/g,
-    (c) =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(
-        16,
-      ),
-  );
-}
-
-function camelize(str) {
-  return str.replace(
-    /(?:^\w|[A-Z]|\b\w)/g,
-    (word, index) => index === 0 ? word.toLowerCase() : word.toUpperCase(),
-  ).replace(/\s+/g, "");
-}
